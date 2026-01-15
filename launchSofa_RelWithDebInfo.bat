19f9ccf3f944ccae4a4f8c8940ccf4c23ec010e1
Fouad Benhaida <f.benhaida@digisurge.fr>
2026-01-15 16:23:33 +0100

    update the path of the sofapython3 and softRobots plugins + print the log in the launching console

--- launchSofa_RelWithDebInfo.bat
+++ launchSofa_RelWithDebInfo.bat
@@ -1,3 +1,9 @@
 @echo off
 set sceneFileRelativePath=%cd%\%1
-start /d "../build/bin/RelWithDebInfo/" runSofa.exe -lSofaPython3 %sceneFileRelativePath% -g batch -n 90
+cd /d "%~dp0..\build\bin\RelWithDebInfo\"
+runSofa.exe -lSofaPython3 %sceneFileRelativePath% -g batch -n 90
+if errorlevel 1 (
+    echo.
+    echo Process exited with error code %errorlevel%
+)
+pause
